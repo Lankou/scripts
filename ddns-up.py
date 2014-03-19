a30ef4f172a9c249fd20c94ced02606fd320e091
Maddie Zhan <maddie.zhan@cynovan.com>
2014-03-19 16:16:34 +0800

    First attempt on logging
    No idea if I have implemented it correctly...

--- ddns-up.py
+++ ddns-up.py
@@ -3,7 +3,8 @@
 import httplib, urllib
 import time, datetime
 import json
-import os, platform
+import os, sys, platform
+import logging
 
 """
 User Settings
@@ -16,46 +17,58 @@ settings = dict(
     record_line = "默认",                           # DO NOT CHANGE!!
 )
 dyndomain = "ddns.example.com"                      # change to your dynamic domain
-update_interval = 60
+update_interval = 60                                # self-explanatory option
+pidfile = '/tmp/ddns-up.pid'                        # pid file
+logfile = '/tmp/ddns-up.log'                        # log file (set to None to disable logging)
 
-###################################################################################
+#####################################################################################################
 # Initialize - you don't need to change this.
 # Only support one level of subdomain at this moment (xxxxx.example.com)
-# TODO: figure out a way to parse domain names correctly other than using extra
+# TODO: figure out a way to parse domain names correctly other than using extra libs like tldextract
 # TODO: add support for updating multiple domains
-# TODO: add logging
-# libs like tldextract
+# TODO: add some kind of log path validation (too lazy)
 domain = dyndomain.split('.', 1)[1]
 subdomain = dyndomain.split('.', 1)[0]
 settings.update(dict(sub_domain=subdomain))
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json"}
+logger = logging.getLogger('DNSPod DDNS Updater')
+if logfile is None:
+    logging.basicConfig(level=logging.INFO)
+    logger.info('Logging is disabled. Sending output to stdout.')
+else:
+    logging.basicConfig(filename=logfile, level=logging.INFO)
 
 def ts():
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return timestamp
 
 if platform.system() == 'Linux':
-    if os.path.isfile('/var/run/ddns-up.pid'):
-        with open ('/var/run/ddns-up.pid', 'r') as pidfile:
-            oldpid = pidfile.read().strip()
+    if not pidfile or not os.path.isfile(pidfile):
+        logger.error('%s: Invalid PID file path: %s. Please check your settings.', ts(), pidfile)
+        sys.exit(1)
+    if os.path.isfile(pidfile):
+        with open (pidfile, 'r') as pf:
+            oldpid = pf.read().strip()
             if os.path.exists('/proc/' + oldpid) and oldpid:
-                raise RuntimeError('%s: Another process %s is still running. Terminating.' % (ts(), oldpid))
+                logger.error('%s: Another process %s is still running. Terminating.', ts(), oldpid)
+                sys.exit(1)
             else:
-                with open('/var/run/ddns-up.pid', 'w') as pidfile:
+                with open(pidfile, 'w') as pf:
                     try:
-                        print '%s: PID file detected but old process is stale. Updating PID file.' % ts()
-                        pidfile.write(str(os.getpid()))
+                        logger.info('%s: PID file detected but old process is stale. Updating PID file.', ts())
+                        pf.write(str(os.getpid()))
                     except:
-                        raise RuntimeError('%s: Unable to create PID file. Make sure you have write access to /var/run/' % ts())
-
+                        logger.error('%s: Unable to create PID file. Make sure you have write access to %s', ts(), pidfile, exc_info=True)
+                        raise
     else:
         try:
-            print '%s: PID file not found. Assuming this is the only instance running.' % ts()
-            print 'Creating PID file with pid %s.' % os.getpid()
-            with open('/var/run/ddns-up.pid', 'w') as pidfile:
-                pidfile.write(str(os.getpid()))
+            logger.info('%s: PID file not found. Assuming this is the only instance running.', ts())
+            logger.info('Creating PID file with pid %s.', os.getpid())
+            with open(pidfile, 'w') as pf:
+                pf.write(str(os.getpid()))
         except:
-            raise RuntimeError('%s: Unable to create PID file. Make sure you have write access to /var/run/' % ts())
+            logger.error('%s: Unable to create PID file. Make sure you have write access to %s', ts(), pidfile, exc_info=True)
+            raise
 
 def getinfo(infotype):
     conn = httplib.HTTPSConnection('dnsapi.cn')
@@ -72,13 +85,15 @@ def getinfo(infotype):
                 if names['name'] == domain:
                     domid = names['id']
                     settings.update(dict(domain_id=domid))
-                    print '%s: ID for domain %s fetched, ID %s' % (ts(), domain, domid)
+                    logger.info('%s: ID for domain %s fetched, ID %s', ts(), domain, domid)
 
             if 'domain_id' not in settings:
-                raise ValueError('%s: Domain %s cannot be found under account %s!' % (ts(), domain, settings.get('login_email')))
+                logger.error('%s: Domain %s cannot be found under account %s!', ts(), domain, settings.get('login_email'))
+                sys.exit(1)
 
         except KeyError, e:
-            raise KeyError('%s: %s field not found in server response. Please check your login information.' % (ts(), e))
+            logger.error('%s: %s field not found in server response. Please check your login information.', ts(), e, exc_info=True)
+            raise
 
     elif infotype == 'recid':
         try:
@@ -92,19 +107,22 @@ def getinfo(infotype):
                 if records['name'] == subdomain:
                     recid = records['id']
                     settings.update(dict(record_id=recid))
-                    print '%s: ID for subdomain %s fetched, ID %s' % (ts(), subdomain, recid)
+                    logger.info('%s: ID for subdomain %s fetched, ID %s', ts(), subdomain, recid)
 
             if 'record_id' not in settings:
-                raise ValueError('%s: Subdomain %s cannot be found under %s!' % (ts(), subdomain, domain))
+                logger.error('%s: Subdomain %s cannot be found under %s!', ts(), subdomain, domain)
+                sys.exit(1)
 
         except KeyError, e:
-            raise KeyError('%s: %s field not found in server response. Please check your domain.' % (ts(), e))
+            logger.error('%s: %s field not found in server response. Please check your domain.', ts(), e, exc_info=True)
+            raise
 
     elif infotype == 'recip':
         conn.request("POST", "/Record.Info", urllib.urlencode(settings), headers)
         recip = json.loads(conn.getresponse().read())['record']['value']
         if not recip:
-            raise ValueError('%s: Failed to obtain record IP for %s.' % (ts(), dyndomain))
+            logger.error('%s: Failed to obtain record IP for %s.', ts(), dyndomain)
+            sys.exit(1)
         else:
             return recip
 
@@ -114,9 +132,11 @@ def updateddns():
     try:
         conn = httplib.HTTPSConnection('dnsapi.cn')
         conn.request("POST", "/Record.Ddns", urllib.urlencode(settings), headers)
-        print conn.getresponse().read()
         if not conn.getresponse().read():
-            raise IOError('%s: Failed to update DDNS record. Please check your network connectivity.' % ts())
+            logger.error('%s: Failed to update DDNS record. Please check your network connectivity.', ts())
+            sys.exit(1)
+        else:
+            logger.info('%s: DDNS record updated.', ts())
     finally:
         conn.close()
 
@@ -126,7 +146,8 @@ def getip():
     myip = conn.getresponse().read().strip()
     conn.close()
     if not myip:
-        raise IOError('%s: Unable to fetch your current IP. Please check your network connectivity.' % ts())
+        logger.error('%s: Unable to fetch your current IP. Please check your network connectivity.', ts())
+        sys.exit(1)
     else:
         return myip
 
@@ -138,12 +159,12 @@ if __name__ == '__main__':
             myip = getip()
             recip = getinfo('recip')
             if recip != myip:
-                print '%s: IP address changed. (Record: %s; Current: %s) -> Updating.' % (ts(), recip, myip)
-                print '%s: Record: %s, updating to: %s' % (ts(), recip, myip)
+                logger.info('%s: IP address changed. (Record: %s; Current: %s) -> Updating.', ts(), recip, myip)
+                logger.info('%s: Record: %s, updating to: %s', ts(), recip, myip)
                 updateddns()
             else:
-                print '%s: IP address not changed (%s). Sleep for %d seconds.' % (ts(), recip, update_interval)
+                logger.info('%s: IP address not changed (%s). Sleep for %d seconds.', ts(), recip, update_interval)
         except Exception, e:
-            print '%s: %s' % (ts(), e)
+            logger.error('%s: %s', ts(), e)
             pass
         time.sleep(update_interval)
